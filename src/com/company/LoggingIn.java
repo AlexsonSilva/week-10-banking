29331b87f0e581e52d1029b4cc89b4c809fd184a
alexsonsilva <21643@student.dorset-college.ie>
2020-11-25 15:29:01 +0000

    commit menu case 1

--- src/com/company/LoggingIn.java
+++ src/com/company/LoggingIn.java
@@ -10,16 +10,25 @@ public class LoggingIn {
             "3: Exit program.\n\n" +
             "> ";
 
-    public LoggingIn(){
+    public LoggingIn() {
         Scanner sc = new Scanner(System.in);
 
+        String inputPin;
         boolean keepRunning = true;
-        while (keepRunning){
+        while (keepRunning) {
             System.out.println(MENU);
             String option = sc.nextLine();
 
-            switch (option){
+            switch (option) {
                 case "1":
+                    System.out.println("Please enter your pin: ");
+                    inputPin = sc.nextLine();
+                    if (inputPin.equals("A1234")){
+                        System.out.println("Welcome");
+                        keepRunning = false;
+                    }else {
+                        System.out.println("Invalid pin, try again");
+                    }
                     break;
                 case "2":
                     break;
